617c26e317ff052a44698283b5b3725a7f28c8a9
Thom Dickson <td3of4@gmail.com>
2024-03-13 20:41:21 -0400

    Update demo

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,21 @@
 open Ipcf
+open Printf
 
-let tokens = Lexer.lex "\\ x y z ()"
+let () = print_endline " === Demo ==="
+let prog = {|( \ x . ( \y . x y ) ) z z|}
+let () = printf "Original program:\n %s\n\n" prog
+let tokens = Lexer.lex prog
+let () = printf "Tokens:\n %s\n\n" @@ [%derive.show: Lexer.t list] tokens
+let parsed = Parser.parse tokens
 
-let () = print_endline @@ [%derive.show: Lexer.t list] tokens
+let () =
+  printf "Parsed expression:\n %s\n\n"
+  @@ [%derive.show: string Parser.terms] parsed
+;;
+
+let evald = Evaluator.reduce parsed
+
+let () =
+  printf "Evaluated expression:\n %s\n\n"
+  @@ [%derive.show: string Parser.terms] evald
+;;
