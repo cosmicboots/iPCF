bea469ccc278f779aa42c84ebb47ccd29b79509c
Thom Dickson <td3of4@gmail.com>
2024-03-17 16:11:30 -0400

    Fix ^D and ^C

--- lib/repl.ml
+++ lib/repl.ml
@@ -8,9 +8,14 @@ Welcome to the iPCF REPL!
 You can exit the REPL with either [exit] or [CTRL+D]
 |};
   let rec loop () =
-    let cmd = Ocamline.read ~prompt:"iPCF>" ~brackets:[ '(', ')' ] () in
+    let cmd =
+      try Ocamline.read ~prompt:"iPCF>" ~brackets:[ '(', ')' ] () with
+      | End_of_file -> "quit"
+      | Stdlib.Sys.Break -> ""
+    in
     match cmd with
     | "quit" -> ()
+    | "" -> loop ()
     | _ ->
       let result = cmd |> Lexer.lex |> Parser.parse |> Evaluator.reduce in
       print_endline @@ [%derive.show: string Parser.terms] result;
