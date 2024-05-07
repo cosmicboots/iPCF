a3896b9d233fe27cce9a34b7c6d59bbd9be26a11
Thom Dickson <td3of4@gmail.com>
2024-05-07 12:23:50 -0400

    Add is normal form intop

--- lib/intops.ml
+++ lib/intops.ml
@@ -1,35 +1,3 @@
-let is_app : 'a. 'a Parser.terms -> 'a Parser.terms = function
-  | Parser.Box (App _) -> Parser.(Box (Const (Bool true)))
-  | _ -> Parser.(Box (Const (Bool false)))
-;;
-
-let is_abs : 'a. 'a Parser.terms -> 'a Parser.terms = function
-  | Parser.Box (Abs _) -> Parser.(Box (Const (Bool true)))
-  | _ -> Parser.(Box (Const (Bool false)))
-;;
-
-let number_of_vars : 'a. 'a Parser.terms -> 'a Parser.terms =
-  fun t ->
-  let rec f : 'a. 'a Parser.terms -> int =
-    fun term ->
-    match term with
-    | Parser.Var _ -> 1
-    | App (t1, t2) -> f t1 + f t2
-    | Abs t -> f t
-    | Box _ -> 0
-    | Const _ -> 0
-    | Succ x -> f x
-    | Pred x -> f x
-    | IfThenElse (c, t, e) -> f c + f t + f e
-    | IsZero x -> f x
-    | Unbox (_, n) -> f n
-    | Fix _ -> 0
-  in
-  match t with
-  | Box t -> Parser.Const (Nat (f t))
-  | _ -> Parser.Const (Nat 0)
-;;
-
 module Operations = struct
   type gt =
     [ `Bool
@@ -44,12 +12,69 @@ module Operations = struct
     | `Var
     ]
 
-  let t =
-    [ "isApp", (is_app, `Arrow (`Box `Var,  `Box (`Ground `Bool)))
+  (* Mutual recursion used to allow nested intensional operations *)
+  let rec t =
+    [ "isApp", (is_app, `Arrow (`Box `Var, `Box (`Ground `Bool)))
     ; "isAbs", (is_abs, `Arrow (`Box `Var, `Box (`Ground `Bool)))
-    ; "numberOfVars", (number_of_vars, `Arrow (`Box `Var, `Ground `Nat))
+    ; "numberOfVars", (number_of_vars, `Arrow (`Box `Var, `Box (`Ground `Nat)))
+    ; "isNormalForm", (is_normal_form, `Arrow (`Box `Var, `Box (`Ground `Bool)))
     ]
+
+  and is_app : 'a. 'a Parser.terms -> 'a Parser.terms = function
+    | Parser.Box (App _) -> Parser.(Box (Const (Bool true)))
+    | _ -> Parser.(Box (Const (Bool false)))
+
+  and is_abs : 'a. 'a Parser.terms -> 'a Parser.terms = function
+    | Parser.Box (Abs _) -> Parser.(Box (Const (Bool true)))
+    | _ -> Parser.(Box (Const (Bool false)))
+
+  and number_of_vars : 'a. 'a Parser.terms -> 'a Parser.terms =
+    fun t ->
+    let rec f : 'a. 'a Parser.terms -> int =
+      fun term ->
+      match term with
+      | Parser.Var _ -> 1
+      | App (t1, t2) -> f t1 + f t2
+      | Abs t -> f t
+      | Box _ -> 0
+      | Const _ -> 0
+      | Succ x -> f x
+      | Pred x -> f x
+      | IfThenElse (c, t, e) -> f c + f t + f e
+      | IsZero x -> f x
+      | Unbox (_, n) -> f n
+      | Fix _ -> 0
+    in
+    match t with
+    | Box t -> Parser.(Box (Const (Nat (f t))))
+    | _ -> Parser.Const (Nat 0)
+
+  (* This function takes [string Parser.terms] which would be the type of closed terms *)
+  and is_normal_form = function
+    | Box e ->
+      let e' =
+        Result.get_ok
+        @@ Evaluator.eval (fun x -> Evaluator.IntOp (fst @@ List.assoc x t)) e
+      in
+      Parser.Box (Const (Bool (e = e')))
+    | _ ->
+      raise
+        (Invalid_argument
+           "If this is reached, the typechecker failed elsewhere")
   ;;
 
   let keys = List.map fst t
 end
+
+let%test "is_normal_form" =
+  (Operations.is_normal_form
+     Parser.(Box (App (Abs (Var None), Const (Bool true))))
+   = Parser.(Box (Const (Bool false))))
+  && Operations.is_normal_form Parser.(Box (Const (Bool true)))
+     = Parser.(Box (Const (Bool true)))
+;;
+
+let%test "number_of_vars" =
+  Operations.number_of_vars Parser.(Box (Abs (Abs (Var None))))
+  = Parser.(Box (Const (Nat 1)))
+;;
