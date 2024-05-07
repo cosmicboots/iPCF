340ff7bb07e72fe8de9054278146b9c6395563dd
Thom Dickson <td3of4@gmail.com>
2024-05-07 12:47:32 -0400

    Revert "Clean up evaluator"
    This reverts commit 03f91d7d91dec6f89f43a20dd4dae231bb6f1206.

--- lib/evaluator.ml
+++ lib/evaluator.ml
@@ -19,6 +19,40 @@ let%test "subst" =
   | _ -> false
 ;;
 
+(** [root_reduction t] applies a single root reduction rule to [t].*)
+let root_reduction =
+  Parser.(
+    function
+    | App (Abs s, t) -> subst s t (* Beta reduction *)
+    | IfThenElse (Const (Bool true), t, _) -> t
+    | IfThenElse (Const (Bool false), _, f) -> f
+    | Unbox (Box m, n) -> subst n m
+    | Fix m -> subst m (Box (Fix m))
+    | Succ (Const (Nat n)) -> Const (Nat (n + 1))
+    | Pred (Const (Nat n)) -> Const (Nat (n - 1))
+    | IsZero (Const (Nat 0)) -> Const (Bool true)
+    | IsZero (Const (Nat _)) -> Const (Bool false)
+    | t -> t)
+;;
+
+(** [redstep t] maps the root reduction steps over the term [t] once. *)
+let rec redstep : 'a. 'a Parser.terms -> 'a Parser.terms =
+  fun t ->
+  let red = root_reduction t in
+  match red with
+  | App (x, y) -> App (redstep x, redstep y)
+  | IfThenElse (c, t, e) -> IfThenElse (redstep c, t, e)
+  | Var _ as v -> v
+  | Const _ as c -> c
+  | Abs s -> Abs (redstep s)
+  | Box m -> Box (redstep m)
+  | Unbox (m, n) -> Unbox (redstep m, redstep n)
+  | Fix m -> Fix (redstep m)
+  | Succ x -> Succ (redstep x)
+  | Pred x -> Pred (redstep x)
+  | IsZero x -> IsZero (redstep x)
+;;
+
 type eval_error =
   | Eval_error of string
   | Not_implemented
@@ -102,4 +136,34 @@ let%test "eval" =
     Result.get_ok @@ eval (fun _ -> raise (Invalid_argument "")) t
   in
   result = Parser.(Const (Nat 5))
+;;
+
+let%test "single reduction step" =
+  let t =
+    Parser.(
+      App (App (Abs (Abs (App (Var (Some None), Var None))), Var "z"), Var "z"))
+  in
+  let result = redstep t in
+  result = App (Abs (App (Var (Some "z"), Var None)), Var "z")
+;;
+
+(** [reduce t] fully reduces the term [t]. *)
+let reduce t =
+  let rec f t =
+    let red = redstep t in
+    (* ppx_deriving eq is used rather than Stdlib.(=) because ppx_deriving eq
+       is a short-circuiting function, which is faster in theory and it's
+       guaranteed not to raise at runtime. *)
+    if Parser.equal_terms String.equal red t then t else f red
+  in
+  f t
+;;
+
+let%test "full reduction" =
+  let t =
+    Parser.(
+      App (App (Abs (Abs (App (Var (Some None), Var None))), Var "z"), Var "z"))
+  in
+  let result = reduce t in
+  result = App (Var "z", Var "z")
 ;;
